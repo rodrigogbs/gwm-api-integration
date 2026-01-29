6b2ee65edb5d47bea6bb175a848d5916c9bfdc69
rodrigogbs <71797145+rodrigogbs@users.noreply.github.com>
2026-01-29 14:39:41 -0300

    Update __init__.py

--- custom_components/haval/__init__.py
+++ custom_components/haval/__init__.py
@@ -1,23 +1,31 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN, PLATFORMS
-from .api import HavalAPI
-from .coordinator import HavalCoordinator
+from .const import DOMAIN
+from .coordinator import HavalDataUpdateCoordinator
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    api = HavalAPI(
-        entry.data["email"],
-        entry.data["password"]
-    )
 
-    await api.login()
-
-    coordinator = HavalCoordinator(hass, api)
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    coordinator = HavalDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        ["sensor", "binary_sensor", "switch", "climate", "device_tracker"],
+    )
+
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        entry,
+        ["sensor", "binary_sensor", "switch", "climate", "device_tracker"],
+    )
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
